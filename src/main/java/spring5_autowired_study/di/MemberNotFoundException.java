f8714823bb41377b05679338b8507729891a0275
Ryong <kjr2436@gmail.com>
2021-05-06 14:21:02 +0900

    spring5_autowired_study

---
+++ src/main/java/spring5_autowired_study/di/MemberNotFoundException.java
@@ -0,0 +1,13 @@
+package spring5_autowired_study.di;
+
+@SuppressWarnings("serial")
+public class MemberNotFoundException extends RuntimeException {
+
+	public MemberNotFoundException() {
+	}
+
+	public MemberNotFoundException(String arg0) {
+		super(arg0);
+	}
+
+}
